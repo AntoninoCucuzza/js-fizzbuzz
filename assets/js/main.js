aa32972a1237013ebb384b29e28159c54acead61
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-08-25 15:15:00 +0200

    add fizz buzz FizzBuzz in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,26 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 */
 
-for(let n = 1; n <= 100; n++){
-    console.log(n);   
-   
+
+
+
+
+for (let n = 1; n <= 100; n++) { //loop da 1 a 100 
+ 
+    if (n % 3 == 0 || n % 5 == 0 ) {// se n multiplo di 3 o 5 entra
+       
+        if (n % 3 == 0 && n % 5 == 0 ) {// se n multiplo di 3 e 5 .log fizzbuzz
+        console.log('FizzBuzz');
+    
+        }else if (n % 3 == 0) {// se n multiplo di 3 .log fizz
+            console.log('Fizz');
+
+        }else if(n % 5== 0) {// se n multiplo di 5 .log buzz
+            console.log('Buzz');
+
+        }
+    }else {//se non ha un multiplo definito .log n
+        console.log(n);
+
+    }
 }
